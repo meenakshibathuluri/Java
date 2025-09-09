773fda833638406cb1ed11ee8af23f1c026eaff7
meenakshibathuluri <meenakshibathuluri@gmail.com>
2025-09-09 10:07:53 -0700

    multipleof3 program uploaded

---
+++ codeChef/Multipleof3.java
@@ -0,0 +1,28 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+       Scanner sc = new Scanner(System.in);
+       int n = sc.nextInt();
+       if(n%3!=0) {
+          int quotient = n/3;
+          int fristMultiple = quotient * 3;
+          int secondMultiple = fristMultiple+3;
+          int fristDiff = n - fristMultiple;
+          int secondDiff = secondMultiple -n;
+         if(fristDiff<secondDiff) {
+           System.out.println(fristMultiple);
+         } else {
+           System.out.println(secondMultiple);
+         } 
+        
+        } else {
+	      System.out.println(n);
+	  }
+	}
+}
