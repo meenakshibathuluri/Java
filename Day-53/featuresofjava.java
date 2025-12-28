abadd9ec0e31b8d1a1b92cdccec23d079775276b
meenakshibathuluri <meenakshibathuluri@gmail.com>
2025-12-28 19:19:01 +0530

    java program uploaded

---
+++ Day-53/featuresofjava.java
@@ -0,0 +1,44 @@
+features of java :
+1.simple : pointers and multiple inheritances.
+pointers means : a pointer is a variables that stores the memory address of another variable.
+multiple inheritances : multiple inheritances is a feature of object oriented programming.
+
+object oriented : deals with the real time object.
+they are 7 type :
+1.class
+2.object
+3.inheritances
+4.encapsulation
+5.interface
+6.abstraction
+7.polymorphism
+
+2.multithreading :
+we can execte multiple threads at a time. multithreading is a programming concept where a single program or process can execute multiple threads or flow.
+
+3.platforms independnt :
+if we write code in any operating system and execte this code in another operating system results will be same
+
+4.WORA : write once and run anywhere.
+
+5.portable :
+when we write a java program the code frist get converted into bytecode and this bytecode does not depend on any operating system or any specific computer.
+we can simply execte this bytecode on any platforms with the help of jvm.
+
+6.robust and security :
+java compiler will show the errors during compiltion. in the  context of software or system robust refers to the ability. 
+security refers to the measures taken to protect a system application and data.
+
+7.scalable : 
+ using java we can build small and large project refers to the ability of a system application or process.
+
+8.intrepreted :
+ jvm is used to intrepret the byte code into machine executable code. 
+ refers to a programming language or code that is executed directly by an interpreter at runtime, without prior compilation into machine code.
+
+9.memory management :
+garabage collectors and deallocate the memory.memory management refer to the process of managing computer memory ensoring that
+1.memory is allocated
+2.memory is deallocated
+3.memory is optimized
+
