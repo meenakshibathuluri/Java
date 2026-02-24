9edd8eb9d6a8df25bc24071344f22eceabed954d
meenakshibathuluri <meenakshibathuluri@gmail.com>
2026-02-24 19:41:51 +0530

    java program uploaded

---
+++ Day-55/Assignment.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class Assignment {
+    public static void main(String[] args) {
+        int a = 20;
+        a += a;
+        System.out.println(a);
+
+         int b = 40;
+         b -= b;
+         System.out.println(b);
+
+         int c = 10;
+         c *= c;
+         System.out.println(c);
+
+         int d = 100;
+         d /= d;
+         System.out.println(d);
+
+         int e = 60;
+         e %= e;
+         System.out.println(e);
+    }
+}
--- Day-55/logical.java
+++ Day-55/logical.java
@@ -10,5 +10,7 @@ public class logical {
         System.out.println((10<5) || (10>5));
 
         System.out.println((10>10) || (10<5));
+
+        System.out.println(!(10>=10));          
     }
 }
