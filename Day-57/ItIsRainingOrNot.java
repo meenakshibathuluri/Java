6bfa7c5bffbf96573e4dc08109692dc7fefc2189
meenakshibathuluri <meenakshibathuluri@gmail.com>
2026-02-26 20:15:37 +0530

    java program uploaded

---
+++ Day-57/ItIsRainingOrNot.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class ItIsRaining {
+    public static void main(String[] args) {
+       boolean itIsRaining = false;
+       if(itIsRaining) {
+           System.out.println("holiday");
+       } else  {
+           System.out.println("no holiday");
+       }
+    }
+}
