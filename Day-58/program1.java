734067ee151dbe249e8e504c5421dbc066b613b3
meenakshibathuluri <meenakshibathuluri@gmail.com>
2026-02-27 19:29:36 +0530

    java program uploaded

--- Day-58/program1.java
+++ Day-58/program1.java
@@ -1,29 +1,29 @@
 import java.util.Scanner;
-public class BiggestAmoungThreeNumbers {
+
+public class BiggestAmongThreeNumbers {
     public static void main(String[] args) {
-          Scanner sc = new Scanner(System.in);
-          System.out.println("enter the values of a:");
-          int a = sc.nextInt();
-          System.out.println("enter the values of b:");
-          int b = sc.nextInt();
-          System.out.println("enter the values of c:");
-          int c = sc.nextInt();
-          if(a>b && a>c) {
-              System.out.println("a is bigger");
-          } else if(b>a && b>c) {
-             System.out.println("b is bigger");
-          } else if(c>a && c>b) {
-              System.out.println("c is bigger");
-          } else if(a==b && b==c && c==a) {
-              System.out.println("a and b and c are equals");
-          } else if(a==b && c!=0) {
-              System.out.println("a and b are equals");
-          } else if(a==c && b!=0) {
-              System.out.println("a and c are equals");
-          } else if(b==c && a!=0) {
-              System.out.println("b and c are equals");
-          } else {
-              System.out.println("a and  b and c are not posiable:");
-          }
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the a value");
+        int a = sc.nextInt();
+        System.out.println("Enter the b value");
+        int b = sc.nextInt();
+        System.out.println("Enter the c value");
+        int c = sc.nextInt();
+
+        if(a>b && a>c) {
+            System.out.println("A is bigger");
+        } else if(b>a && b>c) {
+            System.out.println("B is bigger");
+        } else if(c>a && c>b) {
+            System.out.println("C is bigger");
+        } else if(a==b && a!=c) {
+            System.out.println("A and B are equal");
+        } else if(a==c && a!=b) {
+            System.out.println("A and C are equal");
+        } else if(b==c && b!=a) {
+            System.out.println("B and C are equal");
+        } else {
+            System.out.println("All are equal");
+        }
     }
 }
