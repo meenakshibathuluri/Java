7af71b6e7f2faabd5148de4a446f77553e815354
meenakshibathuluri <meenakshibathuluri@gmail.com>
2026-03-05 19:40:47 +0530

    java program uploaded

---
+++ Day-61/MultiplicationOfTable.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class MultiplicationOfATable {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter the value of n:");
+
+        int n = sc.nextInt();
+        for(int i=1;i<=10;i++) {
+            System.out.println(n + "*" +i + "=" +(n*i));
+        }
+    }
+}
