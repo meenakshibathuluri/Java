dcd39076bffc4bd201461c4e3a9ea402a1f35e3b
meenakshibathuluri <meenakshibathuluri@gmail.com>
2026-03-07 19:33:53 +0530

    java program uploaded

--- Day-62/Patterns2.java
+++ Day-62/Patterns2.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 public class Pattern2 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        
         System.out.println("enter the value of n:");
 
         int n = sc.nextInt();
