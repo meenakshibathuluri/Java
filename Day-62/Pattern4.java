6843e285c81b4b25d4cfbe92d67ddc98953f7f7c
meenakshibathuluri <meenakshibathuluri@gmail.com>
2026-03-08 14:29:33 +0530

    java program uploaded

---
+++ Day-62/Pattern4.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Pattern3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter the values of n: ");
+
+        int n = sc.nextInt();
+        for(int i=0;i<n;i++) {
+            for(int j=0;j<i+1;j++) {
+                System.out.print(j+1);
+            }
+            System.out.println();
+        }
+    }
+}
