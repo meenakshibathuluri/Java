be829ce08738896e2acb20f1f388da047332185f
meenakshibathuluri <meenakshibathuluri@gmail.com>
2026-03-13 19:22:20 +0530

    java program uploaded

--- Day-64/SumOfCountN.java
+++ Day-64/SumOfCountN.java
@@ -2,7 +2,9 @@ import java.util.Scanner;
 public class  SumOfCountN {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        
         System.out.println("Enter the value of n:");
+
         int n = sc.nextInt();
 
         int sum=0;
