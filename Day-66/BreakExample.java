291d76c0a9f43fadc5b9f782936625f51ac39138
meenakshibathuluri <meenakshibathuluri@gmail.com>
2026-03-16 19:25:36 +0530

    java program uploaded

--- Day-66/BreakExample.java
+++ Day-66/BreakExample.java
@@ -1,7 +1,7 @@
 public class BreakExample {
     public static void main(String[] args) {
         for(int i=1;i<=5;i++) {
-            if(i==1) {
+            if(i==5) {
                 break;
             }
             System.out.println(i);
--- Day-66/Continue.java
+++ Day-66/Continue.java
@@ -1,7 +1,7 @@
 public class Continue {
     public static void main(String[] args) {
         for(int i=1;i<=5;i++) {
-            if(i==1) {
+            if(i==5) {
                 continue;
             }
             System.out.println(i);
