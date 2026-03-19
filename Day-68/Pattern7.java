9c6271527e604f4562adb7a059c8466865457887
meenakshibathuluri <meenakshibathuluri@gmail.com>
2026-03-19 20:32:54 +0530

    java program uploaded

--- Day-68/Pattern7.java
+++ Day-68/Pattern7.java
@@ -3,7 +3,6 @@ public class Pattern7 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the value of n:");
-
         int n = sc.nextInt();
         for(int i=0;i<n;i++) {
             for(int k=0;k<i;k++) {
