1ee59ee980985d7089ce020ae62c4c8ea1f55fc8
meenakshibathuluri <meenakshibathuluri@gmail.com>
2026-03-21 20:22:24 +0530

    java program uploaded

--- Day-68/Pattern10.java
+++ Day-68/Pattern10.java
@@ -1,7 +1,9 @@
 import java.util.Scanner;
 public class Pattern10 {
     public static void main(String[] args) {
+
         Scanner sc = new Scanner(System.in);
+        
         System.out.println("Enter the value of n:");
         int n = sc.nextInt();
         for(int k=0;k<n;k++) {
