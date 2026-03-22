0b532b983f0a77802efd492e468c2dca682cdaa0
meenakshibathuluri <meenakshibathuluri@gmail.com>
2026-03-22 19:16:58 +0530

    java program uploaded

--- Day-68/Pattern9.java
+++ Day-68/Pattern9.java
@@ -1,7 +1,9 @@
 import java.util.Scanner;
 public class Pattern9 {
     public static void main(String[] args) {
+
         Scanner sc = new Scanner(System.in);
+        
         System.out.println("Entre the value of n");
         int n = sc.nextInt();
 
