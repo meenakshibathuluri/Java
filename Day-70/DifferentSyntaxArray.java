f704294123a76bbc383ed7b74e7dff4221132765
meenakshibathuluri <meenakshibathuluri@gmail.com>
2026-03-27 19:38:23 +0530

    java program uploaded

---
+++ Day-70/DifferentSyntaxArray.java
@@ -0,0 +1,16 @@
+public class DifferentSyntaxArray {
+    public static void main(String[] args) {
+        int[] arr = {10,20,30};
+
+        System.out.println(arr.length);
+
+        int[] arr1 = new int[6];
+        arr1[0] = 10;
+        arr1[1] = 20;
+        arr1[2] = 30;
+
+        for(int i=0;i< arr.length;i++) {
+            System.out.println(arr1[i]);
+        }
+    }
+}
