e191fc78e15cdf7c02ab050515e59e240b3dfd8e
meenakshibathuluri <meenakshibathuluri@gmail.com>
2026-03-29 19:02:25 +0530

    java program uploaded

--- Day-71/CountZerosOnesAndTwos.java
+++ Day-71/CountZerosOnesAndTwos.java
@@ -5,10 +5,12 @@ public class CountZerosOnesAndTwos {
         Scanner sc = new Scanner(System.in);
 
         System.out.println("Enter the size of the array: ");
+        
         int size = sc.nextInt();
         int[] arr = new int[size];
         
         System.out.println("Enter the array element:");
+
         for(int i=0;i<arr.length;i++) {
             arr[i] = sc.nextInt();
         }
