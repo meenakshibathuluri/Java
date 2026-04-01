a04f856087651808be733baf3ad1fc1d92885750
meenakshibathuluri <meenakshibathuluri@gmail.com>
2026-04-01 19:53:01 +0530

    java program uploaded

--- Day-71/ZerosEndOfTheArray.java
+++ Day-71/ZerosEndOfTheArray.java
@@ -4,9 +4,11 @@ public class ZerosEndOfTheArray {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("enter the size of array: ");
+
         int size = sc.nextInt();
         int[] arr = new int[size];
         System.out.println("enter the array element: ");
+
         for (int i = 0; i < arr.length; i++) {
             arr[i] = sc.nextInt();
         }
