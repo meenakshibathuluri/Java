6ad88fdf0fc223e08ee5803b16a0461a27636d91
meenakshibathuluri <meenakshibathuluri@gmail.com>
2026-04-02 18:48:14 +0530

    java program uploaded

--- Day-71/LinearSearch1.java
+++ Day-71/LinearSearch1.java
@@ -2,10 +2,13 @@ import java.util.Scanner;
 public class LinearSearch1 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
         System.out.println("Enter the size of the array: ");
+
         int size = sc.nextInt();
         int[] arr = new int[size];
         System.out.println("Enter the array elements: ");
+        
         for (int i = 0; i < arr.length; i++) {
             arr[i] = sc.nextInt();
         }
