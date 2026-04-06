53642a22467da1ce3936631fef9ae088b13ed9c6
meenakshibathuluri <meenakshibathuluri@gmail.com>
2026-04-06 19:20:01 +0530

    java program uploaded

--- Day-72/secondLargest.java
+++ Day-72/secondLargest.java
@@ -3,10 +3,13 @@ import java.util.Scanner;
 public class secondLargestInArray {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
         System.out.println("enter the size of array: ");
+
         int n = sc.nextInt();
         int[] arr = new int[n];
         System.out.println("enter" + n + "array element: ");
+        
         for (int i=0;i<=n-1;i++) {
             arr[i] = sc.nextInt();
         }
--- Day-72/selectionSort.java
+++ Day-72/selectionSort.java
@@ -2,10 +2,13 @@ import java.util.Scanner;
 public class selectionsort {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
         System.out.println("enter the size of array: ");
+
         int size = sc.nextInt();
         int[] arr = new int[size];
         System.out.println("enter" + arr.length + "array element: ");
+        
         for (int i = 0; i < arr.length; i++) {
             arr[i] = sc.nextInt();
         }
