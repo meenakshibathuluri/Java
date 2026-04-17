e33f78a8ba1913388494a69cc673b8f8474e3bfd
meenakshibathuluri <meenakshibathuluri@gmail.com>
2026-04-17 19:26:10 +0530

    java program uploaded

--- Day-73/Insertion.java
+++ Day-73/Insertion.java
@@ -4,11 +4,16 @@ import java.util.Scanner;
 
 public class InsertionArray2 {
     public static void main(String[] args){
+
         Scanner sc=new Scanner(System.in);
+
         System.out.println("Enter the size of array:");
+
         int size= sc.nextInt();
         int []arr=new int[size];
+
         System.out.println("Enter the elements of array:");
+        
         for (int i=0;i< arr.length;i++){
             arr[i]= sc.nextInt();
         }
@@ -18,6 +23,7 @@ public class InsertionArray2 {
             System.out.println(arr[i]);
         }
     }
+
     static void InsertionArray(int [] arr){
         for (int i=0;i< arr.length;i++){
             int current=arr[i];
