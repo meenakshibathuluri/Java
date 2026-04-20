ce07424b49322f75b8fe6798ba3e506be2f7ab27
meenakshibathuluri <meenakshibathuluri@gmail.com>
2026-04-20 19:36:12 +0530

    java program uploaded

--- Day-73/Insertion.java
+++ Day-73/Insertion.java
@@ -3,7 +3,9 @@ package methods;
 import java.util.Scanner;
 
 public class InsertionArray2 {
+
     public static void main(String[] args){
+        
         Scanner sc=new Scanner(System.in);
         System.out.println("Enter the size of array:");
         int size= sc.nextInt();
