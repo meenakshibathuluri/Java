7705841c91fffb8c6bc0d24c1b4c71eec5a75b17
meenakshibathuluri <meenakshibathuluri@gmail.com>
2026-07-17 19:47:38 +0530

    java program uploaded

--- Day-73/sumOfArray.java
+++ Day-73/sumOfArray.java
@@ -1,18 +1,18 @@
 import java.util.Scanner;
 public class SumOfArrayElements {
   public static void main(String[] args) {
-     Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the size of the array: ");
-    int size = sc.nextInt();
-    int[] arr = new int[size];
-       System.out.println("Enter the array elements: ");
-    for(int i=0;i<arr.length;i++) {
-       arr[i] = sc.nextInt();
-    } 
-     int sum = 0;
-    for(int i=0;i<arr.length;i++) {
-       sum += arr[i];
-        System.out.println(sum);
-    }
-  }
+      Scanner sc = new Scanner(System.in);
+      System.out.println("Enter the size of the array: ");
+         int size = sc.nextInt();
+         int[] arr = new int[size];
+            System.out.println("Enter the array elements: ");
+         for(int i=0;i<arr.length;i++) {
+            arr[i] = sc.nextInt();
+         } 
+         int sum = 0;
+         for(int i=0;i<arr.length;i++) {
+            sum += arr[i];
+            System.out.println(sum);
+         }
+   }
 }
