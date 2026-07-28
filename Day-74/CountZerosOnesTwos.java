30c39ddd9d52477cff1f5da7be0628e56bbeaefb
meenakshibathuluri <meenakshibathuluri@gmail.com>
2026-07-28 19:24:00 +0530

    java program uploaded

--- Day-74/CountZerosOnesTwos.java
+++ Day-74/CountZerosOnesTwos.java
@@ -10,9 +10,9 @@ public class CountZerosOnesTwos {
            arr[i] = sc.nextInt();
         }
         
-        int zeros = 0;
-        int ones = 0;
-        int twos = 0;
+          int zeros = 0;
+          int ones = 0;
+          int twos = 0;
         for(int i=0;i<arr.length;i++) {
             if(arr[i] == 0) {
                 zeros++;
