3abeb65f410f2aa2b8f296b7d2e12057a57c3b77
meenakshibathuluri <meenakshibathuluri@gmail.com>
2026-08-01 19:16:13 +0530

    java program uploaded

---
+++ Day-74/program.java
@@ -0,0 +1,33 @@
+1st program
+class ABC {
+    public static void main(String args[]) {
+       System.out.println("hello world");
+    }
+}
+
+2nd program
+class ABC {
+  public static void main(String args[]) {
+    int a=100;
+    int b=200;
+    int c=a+b;
+       System.out.println(c);
+  }
+}
+program 3:
+class ABC  {
+    public static void main(String args[])  {
+        int a=75;
+        int b=20;
+        int c=a+b;
+           System.out.println(c);
+        c=a-b;
+           System.out.println(c);
+        c=a*b;
+           System.out.println(c);
+        c=a/b;
+           System.out.println(c);
+        c=a%b;
+           System.out.println(c);
+    }
+}
