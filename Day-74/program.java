7590b2927c80dcf954f613e6c6224d31a8a3f7ed
meenakshibathuluri <meenakshibathuluri@gmail.com>
2026-08-11 19:21:55 +0530

    java program uploaded

--- Day-74/program.java
+++ Day-74/program.java
@@ -37,7 +37,7 @@ program 4:
 class ABC {
    public static void main(String args[]) {
       int a=10;
-      int b=++a + a--;
+      int b=++a + a-- - --a;
         System.out.println(a);
         System.out.println(b);
    }
