c64f9763ff48e088fe3f30921ea44b5368f9227b
meenakshibathuluri <meenakshibathuluri@gmail.com>
2026-09-10 19:31:43 +0530

    java program uploaded

--- Day-75/CountDigits.java
+++ Day-75/CountDigits.java
@@ -9,6 +9,8 @@ public class CountDigits {
         String word = sc.nextLine();
 
         int digitsCount = 0;
+
+        
 method1:
         for(int i=0;i<word.length();i++) {
             if(word.charAt(i)>=48 && word.charAt(i)<=57) {
@@ -16,6 +18,8 @@ method1:
             }
         }
 // 0 - 48 1 = 49,2 = 50, 3 = 51, 4 = 52, 5 = 53, 6 = 54, 7 = 55, 8 = 56, 9 = 57
+
+
 method2:
         //   for(int i=0;i<word.length();i++) {
         //   if(word.charAt(i)=='0' || word.charAt(i)=='1' || word.charAt(i)=='2' || word.charAt(i)=='3' || word.charAt(i)=='4' || word.charAt(i)=='5' || word.charAt(i)=='6' || word.charAt(i)=='7' || word.charAt(i)=='8' || word.charAt(i)=='9') {
